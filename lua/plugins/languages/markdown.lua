5595df739b2687e872fda89e994e7607044b248c
leesanghyeok <1004xlsrn@gmail.com>
2026-07-18 16:38:17 +0900

    Revert "Tame Mermaid diagram rendering"
    This reverts commit a2601fdd48a51c01286771827034b98eda685687.

--- lua/plugins/languages/markdown.lua
+++ lua/plugins/languages/markdown.lua
@@ -78,15 +78,26 @@ return {
     },
     opts = {
       events = {
-        -- focus 이동마다 mmdc가 다시 돌지 않도록 저장/읽기 시점 위주로만 렌더링한다.
-        render_buffer = { "BufReadPost", "BufWritePost" },
-        clear_buffer = { "BufDelete", "BufWipeout" },
+        -- markdown을 열면 Mermaid block을 자동으로 렌더링한다.
+        -- <leader>cm은 커서 위치 diagram을 별도 탭에서 크게 볼 때 쓴다.
+        render_buffer = { "BufWinEnter", "InsertLeave", "TextChanged" },
+        clear_buffer = { "BufLeave" },
       },
       renderer_options = {
         mermaid = {
           theme = "dark",
           scale = 2,
         },
+      },
+    },
+    keys = {
+      {
+        "<leader>cm",
+        function()
+          require("diagram").show_diagram_hover()
+        end,
+        ft = "markdown",
+        desc = "Markdown Mermaid Diagram",
       },
     },
   },
