8d706a45f3887e87a689c7b4f6c6b11fb7bf8b6e
leesanghyeok <1004xlsrn@gmail.com>
2026-07-18 15:41:27 +0900

    Disable spellcheck for text files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -31,3 +31,29 @@ vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
     end
   end,
 })
+
+local text_filetypes = {
+  gitcommit = true,
+  markdown = true,
+  plaintex = true,
+  text = true,
+  typst = true,
+}
+
+local function disable_text_spell(buf)
+  if text_filetypes[vim.bo[buf].filetype] then
+    -- LazyVim은 문서 파일에서 영어 spellcheck를 자동으로 켠다.
+    -- 한글을 많이 쓰면 모든 한글 단어가 오타처럼 밑줄 표시되므로 spell만 끈다.
+    vim.opt_local.spell = false
+  end
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("StarkDisableTextSpell", { clear = true }),
+  pattern = vim.tbl_keys(text_filetypes),
+  callback = function(event)
+    disable_text_spell(event.buf)
+  end,
+})
+
+disable_text_spell(0)
