4ad6ad0e32c24ed1052b50dd9a0641c2e89c1254
leesanghyeok <1004xlsrn@gmail.com>
2026-07-22 22:59:00 +0900

    commit

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,12 +29,12 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.ai.copilot" },
-    { import = "lazyvim.plugins.extras.ai.avante" },
+    -- { import = "lazyvim.plugins.extras.ai.copilot" },
+    -- { import = "lazyvim.plugins.extras.ai.avante" },
     -- import/override with your plugins
 
     --- UI
@@ -56,9 +56,9 @@ require("lazy").setup({
     { import = "plugins.languages.yaml" },
 
     --- AI
-    { import = "plugins.ai.avante" },
-    { import = "plugins.ai.codecompanion" },
-    { import = "plugins.ai.copilot" },
+    -- { import = "plugins.ai.avante" },
+    -- { import = "plugins.ai.codecompanion" },
+    -- { import = "plugins.ai.copilot" },
 
     --- Integrations
     { import = "plugins.integrations.mise" },
